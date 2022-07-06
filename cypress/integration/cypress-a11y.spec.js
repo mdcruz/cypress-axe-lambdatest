a554b1ccd0f0377fd790925721dc730420f748af
Marie Cruz <marie.descruz@gmail.com>
2022-07-06 14:47:05 +0100

    commands to run cypress tests headlessly

--- cypress/integration/cypress-a11y.spec.js
+++ cypress/integration/cypress-a11y.spec.js
@@ -6,12 +6,12 @@ describe('Accessibility', () => {
     cy.injectAxe();
 
     // scans the entire page by default
-    // cy.checkA11y();
+    cy.checkA11y();
 
     // scans the specific selector only
     // cy.checkA11y('#entry_217838 p');
 
     // exclude specific elements
-    cy.checkA11y({ exclude: ['#entry_217838 p'] });
+    // cy.checkA11y({ exclude: ['#entry_217838 p'] });
   });
 });
